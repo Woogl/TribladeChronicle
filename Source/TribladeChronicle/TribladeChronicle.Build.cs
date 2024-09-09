a2bc411025e5b0cee06f79bf7e810218c640b555
Woogl <106436076+Woogl@users.noreply.github.com>
2024-09-09 20:32:55 +0900

    Unreal project default commit

---
+++ Source/TribladeChronicle/TribladeChronicle.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TribladeChronicle : ModuleRules
+{
+	public TribladeChronicle(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", });
+	}
+}
