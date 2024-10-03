daebcaa2c7defe4f95146c2b5681af23fbe56e91
Woogl <106436076+Woogl@users.noreply.github.com>
2024-10-03 15:49:16 +0900

    Prevent friendly fire with Gameplay Targeting System

--- Plugins/TcTargetingSystem/Source/TcTargetingSystem/Private/TcTargetingFilterTask_TeamID.cpp
+++ Plugins/TcTargetingSystem/Source/TcTargetingSystem/Private/TcTargetingFilterTask_TeamID.cpp
@@ -3,3 +3,71 @@
 
 #include "TcTargetingFilterTask_TeamID.h"
 
+#include "GenericTeamAgentInterface.h"
+
+bool UTcTargetingFilterTask_TeamID::ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle, const FTargetingDefaultResultData& TargetData) const
+{
+	FGenericTeamId TargetActorTeamID = FGenericTeamId::NoTeam;
+	if (AActor* TargetActor = TargetData.HitResult.GetActor())
+	{
+		TargetActorTeamID = FindTeamID(TargetActor);
+	}
+
+	FGenericTeamId SourceActorTeamID = FGenericTeamId::NoTeam;
+	if (const FTargetingSourceContext* SourceContext = FTargetingSourceContext::Find(TargetingHandle))
+	{
+		if (SourceContext->SourceActor)
+		{
+			SourceActorTeamID = FindTeamID(SourceContext->SourceActor);
+		}
+	}
+	
+	if (AActor* TargetActor = TargetData.HitResult.GetActor())
+	{
+		// if the target is one of these IDs, filter it out
+		for (uint8 IDFilter : IgnoredTeamIDs)
+		{
+			if (IDFilter == TargetActorTeamID)
+			{
+				return true;
+			}
+		}
+
+		// if the target is one of these IDs, do not filter it out
+		for (uint8 IDFilter : RequiredTeamIDs)
+		{
+			if (IDFilter == TargetActorTeamID)
+			{
+				return false;
+			}
+		}
+
+		if (bIgnoreSourceActorTeamID)
+		{
+			if (SourceActorTeamID == TargetActorTeamID)
+			{
+				return true;
+			}
+		}
+
+		// if we do not have required class IDs, we do NOT want to filter this target
+		return (RequiredTeamIDs.Num() > 0);
+	}
+
+	return true;
+}
+
+FGenericTeamId UTcTargetingFilterTask_TeamID::FindTeamID(AActor* TargetActor) const
+{
+	if (APawn* Pawn = Cast<APawn>(TargetActor))
+	{
+		if (AController* Controller = Pawn->GetController())
+		{
+			if (IGenericTeamAgentInterface* Interface = Cast<IGenericTeamAgentInterface>(Controller))
+			{
+				return Interface->GetGenericTeamId();
+			}
+		}
+	}
+	return FGenericTeamId::NoTeam;
+}
--- Plugins/TcTargetingSystem/Source/TcTargetingSystem/Public/TcTargetingFilterTask_TeamID.h
+++ Plugins/TcTargetingSystem/Source/TcTargetingSystem/Public/TcTargetingFilterTask_TeamID.h
@@ -3,15 +3,34 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Tasks/TargetingTask.h"
+#include "GenericTeamAgentInterface.h"
+#include "Tasks/TargetingFilterTask_BasicFilterTemplate.h"
 #include "TcTargetingFilterTask_TeamID.generated.h"
 
 /**
  * 
  */
-UCLASS()
-class TCTARGETINGSYSTEM_API UTcTargetingFilterTask_TeamID : public UTargetingTask
+UCLASS(Blueprintable)
+class TCTARGETINGSYSTEM_API UTcTargetingFilterTask_TeamID : public UTargetingFilterTask_BasicFilterTemplate
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual bool ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle, const FTargetingDefaultResultData& TargetData) const override;
+
+protected:
+	/** The set of required team IDs (must be one of these IDs to not be filtered out) */
+	UPROPERTY(EditAnywhere, Category = "Targeting Filter | Class", Meta = (AllowAbstract=true))
+	TSet<uint8> RequiredTeamIDs;
+
+	/** The set of ignored team IDs (must NOT be one of these IDs) */
+	UPROPERTY(EditAnywhere, Category = "Targeting Filter | Class", Meta = (AllowAbstract = true))
+	TSet<uint8> IgnoredTeamIDs;
+
+	/** Add source actor's team ID to "IgnoredTeamIDs" */
+	UPROPERTY(EditAnywhere, Category = "Targeting Filter | Class", Meta = (AllowAbstract=true))
+	bool bIgnoreSourceActorTeamID = true;
+
+private:
+	FGenericTeamId FindTeamID(AActor* TargetActor) const;
 };
